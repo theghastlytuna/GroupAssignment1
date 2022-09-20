2bbeea57a6187f55998f5165ee5a34056aaeb338
Haider Ali <Haiderali360@gmail.com>
2022-09-20 13:03:17 -0400

    Bumper Script Added (Ass Cheeks)

---
+++ Assets/Scripts/BumperScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumperScript : MonoBehaviour
+{
+    [SerializeField] string ignoreTag;
+    [SerializeField] float bounceForce;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision!");
+        if(collision.transform.tag != ignoreTag)
+        {
+            Debug.Log("BOOM!");
+            Rigidbody otherRB = collision.rigidbody;
+            otherRB.AddExplosionForce(bounceForce, collision.contacts[0].point, 5, 30);
+        }
+    }
+}
