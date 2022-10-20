4965bece31e3fe01a1c35161df3a181a47faa2c3
Haider Ali <Haiderali360@gmail.com>
2022-10-20 17:09:06 -0400

    Updated for demonstration

--- Assets/Scripts/Command/InputSupervisor.cs
+++ Assets/Scripts/Command/InputSupervisor.cs
@@ -4,23 +4,40 @@ using UnityEngine;
 
 public class InputSupervisor : MonoBehaviour
 {
-    CommandAbstract[] commands = new CommandAbstract[100];
+    CommandAbstract[] commands = new CommandAbstract[4];
     public JumpCommand jumpCommand;
 
     private void Start()
     {
-        commands[0] = jumpCommand;
+        commands[1] = jumpCommand;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            //Debug.Log("U Key Pressed");
+            if(commands[0] == null) return;
+            commands[0].execute();
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            //Debug.Log("I Key Pressed");
+            if(commands[1] == null) return;
+            commands[1].execute();
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            //Debug.Log("O Key Pressed");
+            if(commands[2] == null) return;
+            commands[2].execute();
+        }
         if (Input.GetKeyDown(KeyCode.P))
         {
             //Debug.Log("P Key Pressed");
-            commands[0].execute();
+            if(commands[3] == null) return;
+            commands[3].execute();
         }
-        //Debug.Log("Loop");
-        //commands[0].execute();
     }
 }
